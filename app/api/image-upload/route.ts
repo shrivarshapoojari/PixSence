a29ec8f9a22fd7e69b9d49eed4dff870467708a6
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-08-31 21:14:19 +0530

    create video upload

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -6,7 +6,7 @@ import { auth } from '@clerk/nextjs/server';
 cloudinary.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+    api_secret: process.env.CLOUDINARY_API_SECRET  
 });
 
 interface CloudinaryUploadResult {
